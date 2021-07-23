1eac88a3d4aa10ad620831ff72453f95a91a034b
Emily Tomas <emily.tomasss@gmail.com>
2021-07-23 11:24:14 -0400

    Primeira Versão

---
+++ MVC-AUTENTICAR/Controllers/AutenticacaoController.cs
@@ -0,0 +1,60 @@
+﻿using MVC_AUTENTICAR.Models;
+using MVC_AUTENTICAR.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_AUTENTICAR.Controllers
+{
+    public class AutenticacaoController : Controller
+    {
+        private UsuariosContext db = new UsuariosContext();
+        // GET: Autenticacao
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+        public ActionResult Login(string ReturnUrl)
+        {
+            var viewmodel = new LoginViewModel
+            {
+                UrlRetorno = ReturnUrl
+            };
+
+            return View(viewmodel);
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(CadastroUsuarioViewModel viewmodel)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(viewmodel);
+            }
+
+           
+            Usuario novoUsuario = new Usuario
+            {
+                Nome = viewmodel.Nome,
+                Login = viewmodel.Login,
+                Senha = viewmodel.Senha
+            };
+
+            db.Usuarios.Add(novoUsuario);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+
+
+        }            
+
+        }
+    }
---
+++ MVC-AUTENTICAR/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(MVC_AUTENTICAR.Startup))]
+
+namespace MVC_AUTENTICAR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/Autenticacao/Login")
+            });
+        }
+    }
+}
